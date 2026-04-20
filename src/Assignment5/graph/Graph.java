c62fb5ac59892a0b9531c08e9b1e54d7d0a3667d
Akshay CU <akshaycu74@gmail.com>
2026-04-20 20:30:02 +0100

    Added initial implementation using adjacency list

--- src/Assignment5/graph/Graph.java
+++ src/Assignment5/graph/Graph.java
@@ -1,5 +1,23 @@
 package Assignment5.graph;
 
-public class Graph {
+import java.util.*;
 
+public class Graph {
+    private Map<Node, List<Edge>> adjList;
+
+    public Graph() {
+        adjList = new HashMap<>();
+    }
+
+    public void addNode(Node node) {
+        adjList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(Node from, Node to, int weight) {
+        adjList.get(from).add(new Edge(to, weight));
+    }
+
+    public Map<Node, List<Edge>> getAdjList() {
+        return adjList;
+    }
 }

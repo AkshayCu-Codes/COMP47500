f557db632fa6a1f1a20e98c1129b27d021f72c84
Akshay CU <akshaycu74@gmail.com>
2026-04-20 19:21:26 +0100

    Node and Edge Implementation Added

--- src/Assignment5/graph/Edge.java
+++ src/Assignment5/graph/Edge.java
@@ -1,5 +1,19 @@
 package Assignment5.graph;
 
 public class Edge {
+    private Node target;
+    private int weight;
 
+    public Edge(Node target, int weight) {
+        this.target = target;
+        this.weight = weight;
+    }
+
+    public Node getTarget() {
+        return target;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
--- src/Assignment5/graph/Node.java
+++ src/Assignment5/graph/Node.java
@@ -1,5 +1,18 @@
 package Assignment5.graph;
 
 public class Node {
+    private int id;
 
+    public Node(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Node-" + id;
+    }
 }

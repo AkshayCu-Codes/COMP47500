7cc0c333c4c810008cfd69aa7197d4cdc5cb2a88
Akshay CU <akshaycu74@gmail.com>
2026-04-21 09:35:15 +0100

    added basic Dijkstra class structure

---
+++ src/Assignment5/algorithms/Dijkstra.java
@@ -0,0 +1,11 @@
+package Assignment5.algorithms;
+
+import Assignment5.graph.*;
+import java.util.*;
+
+public class Dijkstra {
+
+    public Map<Node, Integer> shortestPath(Graph graph, Node source) {
+        return new HashMap<>();
+    }
+}

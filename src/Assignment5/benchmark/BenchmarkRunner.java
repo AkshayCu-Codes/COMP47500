e368b00ca5bf097da66f2cbb5ac11991149a9666
sharan <shrnsrnvsn14@gmail.com>
2026-05-02 19:51:41 +0100

    added comparison to A*

--- src/Assignment5/benchmark/BenchmarkRunner.java
+++ src/Assignment5/benchmark/BenchmarkRunner.java
@@ -2,10 +2,10 @@ package Assignment5.benchmark;
 
 import Assignment5.algorithms.BFS;
 import Assignment5.algorithms.Dijkstra;
+import Assignment5.algorithms.AStar;
 import Assignment5.graph.Graph;
 import Assignment5.graph.Node;
 import Assignment5.utils.GraphGenerator;
-
 
 import java.util.List;
 
@@ -15,44 +15,29 @@ public class BenchmarkRunner {
 
         int[] sizes = {10, 50, 100, 150, 250, 500, 750, 1000};
 
-        System.out.println("BFS Benchmark Results");
-        System.out.println("---------------------------------------------------------------");
-        System.out.printf("%-10s %-12s %-12s %-20s%n",
-                "Nodes", "ExtraEdges", "PathLength", "ExecutionTime(ns)");
-        System.out.println("---------------------------------------------------------------");
+        String line = getLine(155);
 
+        System.out.println("Table: Performance Comparison of BFS, Dijkstra, and A* Algorithms\n");
+        System.out.println(line);
+
+        System.out.printf("%-6s | %-5s | %-14s | %-18s | %-12s | %-8s | %-14s | %-8s | %-12s | %-18s | %-12s%n",
+                "Nodes", "Edges", "BFS Time (ms)", "Dijkstra Time (ms)", "A* Time (ms)",
+                "BFS Cost", "Dijkstra Cost", "A* Cost",
+                "BFS Visited", "Dijkstra Visited", "A* Visited");
+
+        System.out.println(line);
+
+        // data rows
         for (int size : sizes) {
-            runBFSBenchmark(size);
+            runBenchmark(size);
         }
-
-        System.out.println("---------------------------------------------------------------");
+        System.out.println(line);
     }
 
-    private static void runBFSBenchmark(int nodeCount) {
-        int extraEdges = nodeCount / 2;
-
-        Graph graph = GraphGenerator.generateGraph(nodeCount, extraEdges);
-
-        Node start = new Node(1);
-        Node goal = new Node(nodeCount);
-
-        // JVM warm-up
-        for (int i = 0; i < 5; i++) {
-            BFS.findPath(graph, start, goal);
-        }
-
-        // actual measurement
-        long startTime = System.nanoTime();
-        List<Node> path = BFS.findPath(graph, start, goal);
-        long endTime = System.nanoTime();
-
-        System.out.printf("%-10d %-12d %-12d %-20d%n",
-                nodeCount,
-                extraEdges,
-                path.size(),
-                (endTime - startTime));
+    private static String getLine(int length) {
+        return "-".repeat(length);
     }
-    
+
     private static void runBenchmark(int nodeCount) {
         int extraEdges = nodeCount / 2;
 
@@ -65,29 +50,36 @@ public class BenchmarkRunner {
         for (int i = 0; i < 5; i++) {
             BFS.findPath(graph, start, goal);
             Dijkstra.findShortestPath(graph, start, goal);
+            AStar.findPath(graph, start, goal);
         }
 
-        // BFS timing
         long bfsStart = System.nanoTime();
         List<Node> bfsPath = BFS.findPath(graph, start, goal);
         long bfsEnd = System.nanoTime();
 
-        int bfsCost = Dijkstra.getPathCost(graph, bfsPath);
-
-        // Dijkstra timing
         long dijStart = System.nanoTime();
         List<Node> dijPath = Dijkstra.findShortestPath(graph, start, goal);
         long dijEnd = System.nanoTime();
 
-        int dijCost = Dijkstra.getPathCost(graph, dijPath);
+        long astarStart = System.nanoTime();
+        List<Node> astarPath = AStar.findPath(graph, start, goal);
+        long astarEnd = System.nanoTime();
 
-        System.out.printf("%-8d %-12d %-15d %-15d %-15d %-15d%n",
+        int bfsCost = Dijkstra.getPathCost(graph, bfsPath);
+        int dijCost = Dijkstra.getPathCost(graph, dijPath);
+        int astarCost = Dijkstra.getPathCost(graph, astarPath);
+
+        System.out.printf("%-6d | %-5d | %-14.4f | %-18.4f | %-12.4f | %-8d | %-14d | %-8d | %-12d | %-18d | %-12d%n",
                 nodeCount,
                 extraEdges,
-                (bfsEnd - bfsStart),
-                (dijEnd - dijStart),
+                (bfsEnd - bfsStart) / 1_000_000.0,
+                (dijEnd - dijStart) / 1_000_000.0,
+                (astarEnd - astarStart) / 1_000_000.0,
                 bfsCost,
-                dijCost);
+                dijCost,
+                astarCost,
+                BFS.getVisitedNodes(),
+                Dijkstra.getVisitedNodes(),
+                AStar.getVisitedNodes());
     }
-
 }

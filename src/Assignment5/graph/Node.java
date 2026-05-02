706b2841d4148ef493450c67053f55fd001dfe52
AKSHAY CU <akshaycu74@gmail.com>
2026-05-02 20:49:54 +0100

    Add coordinates to Node class
    Added x and y coordinates to Node class with corresponding getters.

--- src/Assignment5/graph/Node.java
+++ src/Assignment5/graph/Node.java
@@ -2,20 +2,36 @@ package Assignment5.graph;
 
 public class Node {
     private int id;
+    private int x;
+    private int y;
 
     public Node(int id) {
+        this(id, 0, 0);
+    }
+
+    public Node(int id, int x, int y) {
         this.id = id;
+        this.x = x;
+        this.y = y;
     }
 
     public int getId() {
         return id;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     @Override
     public String toString() {
         return "Node-" + id;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;

028c8201bc209c3fa3d7180677197fa3df17c95c
sharan <shrnsrnvsn14@gmail.com>
2026-05-02 19:48:20 +0100

    benchmark code for BFS

--- src/Assignment5/benchmark/BenchmarkRunner.java
+++ src/Assignment5/benchmark/BenchmarkRunner.java
@@ -1,5 +1,53 @@
 package Assignment5.benchmark;
 
+import Assignment5.algorithms.BFS;
+import Assignment5.graph.Graph;
+import Assignment5.graph.Node;
+import Assignment5.utils.GraphGenerator;
+
+import java.util.List;
+
 public class BenchmarkRunner {
 
+    public static void main(String[] args) {
+
+        int[] sizes = {10, 50, 100, 150, 250, 500, 750, 1000};
+
+        System.out.println("BFS Benchmark Results");
+        System.out.println("---------------------------------------------------------------");
+        System.out.printf("%-10s %-12s %-12s %-20s%n",
+                "Nodes", "ExtraEdges", "PathLength", "ExecutionTime(ns)");
+        System.out.println("---------------------------------------------------------------");
+
+        for (int size : sizes) {
+            runBFSBenchmark(size);
+        }
+
+        System.out.println("---------------------------------------------------------------");
+    }
+
+    private static void runBFSBenchmark(int nodeCount) {
+        int extraEdges = nodeCount / 2;
+
+        Graph graph = GraphGenerator.generateGraph(nodeCount, extraEdges);
+
+        Node start = new Node(1);
+        Node goal = new Node(nodeCount);
+
+        // JVM warm-up
+        for (int i = 0; i < 5; i++) {
+            BFS.findPath(graph, start, goal);
+        }
+
+        // actual measurement
+        long startTime = System.nanoTime();
+        List<Node> path = BFS.findPath(graph, start, goal);
+        long endTime = System.nanoTime();
+
+        System.out.printf("%-10d %-12d %-12d %-20d%n",
+                nodeCount,
+                extraEdges,
+                path.size(),
+                (endTime - startTime));
+    }
 }

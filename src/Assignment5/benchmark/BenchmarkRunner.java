365999154e7e705724e22a078db0f28851ac20a0
sharan <shrnsrnvsn14@gmail.com>
2026-05-02 19:50:17 +0100

    Added benchmark comparison between BFS and Dijkstra

--- src/Assignment5/benchmark/BenchmarkRunner.java
+++ src/Assignment5/benchmark/BenchmarkRunner.java
@@ -1,9 +1,11 @@
 package Assignment5.benchmark;
 
 import Assignment5.algorithms.BFS;
+import Assignment5.algorithms.Dijkstra;
 import Assignment5.graph.Graph;
 import Assignment5.graph.Node;
 import Assignment5.utils.GraphGenerator;
+
 
 import java.util.List;
 
@@ -50,4 +52,42 @@ public class BenchmarkRunner {
                 path.size(),
                 (endTime - startTime));
     }
+    
+    private static void runBenchmark(int nodeCount) {
+        int extraEdges = nodeCount / 2;
+
+        Graph graph = GraphGenerator.generateGraph(nodeCount, extraEdges);
+
+        Node start = new Node(1);
+        Node goal = new Node(nodeCount);
+
+        // JVM warm-up
+        for (int i = 0; i < 5; i++) {
+            BFS.findPath(graph, start, goal);
+            Dijkstra.findShortestPath(graph, start, goal);
+        }
+
+        // BFS timing
+        long bfsStart = System.nanoTime();
+        List<Node> bfsPath = BFS.findPath(graph, start, goal);
+        long bfsEnd = System.nanoTime();
+
+        int bfsCost = Dijkstra.getPathCost(graph, bfsPath);
+
+        // Dijkstra timing
+        long dijStart = System.nanoTime();
+        List<Node> dijPath = Dijkstra.findShortestPath(graph, start, goal);
+        long dijEnd = System.nanoTime();
+
+        int dijCost = Dijkstra.getPathCost(graph, dijPath);
+
+        System.out.printf("%-8d %-12d %-15d %-15d %-15d %-15d%n",
+                nodeCount,
+                extraEdges,
+                (bfsEnd - bfsStart),
+                (dijEnd - dijStart),
+                bfsCost,
+                dijCost);
+    }
+
 }
